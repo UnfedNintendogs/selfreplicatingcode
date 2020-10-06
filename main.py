aed9bb5fd0f269cf75b010e1a2935e2677a1be27
WillMeinert <will_zim@meinert.com.au>
2020-10-06 18:29:25 +1100

    Added code

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 I = open(__file__).read()
 import random,subprocess,os
 NAME = str(random.randint(1,1000000))
+NAME2 = str(random.randint(1,1000000))
 with open(NAME+'.py','w') as f: # our new pyhton file
+    f.write(I)
+with open(NAME2+'.py','w') as f: # our new pyhton file
     f.write(I)
 my_file = '{}.py'.format(NAME)
 process = subprocess.Popen(["python", my_file], shell =False)
